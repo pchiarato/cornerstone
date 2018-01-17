b2be79ffc4ed4d5a0e84934771e9da8496d2ac70
ghetolay <ghetolay@gmail.com>
2018-01-17 23:58:26 +0100

    fix bug where we assumes pixeldata was whole buffer.
    When converting the pixeldata into a different typed array, we were always using the full buffer.

--- src/image/webgl_renderer.ts
+++ src/image/webgl_renderer.ts
@@ -2,38 +2,43 @@ import { Provider } from '@angular/core';
 import { Image } from './';
 import { ImageRendererWebgl, IMAGE_RENDERER_WEBGL } from '../renderer/webgl';
 
+function buildTexture16(gl: WebGLRenderingContext, image: Image) {
+    gl.pixelStorei(gl.UNPACK_ALIGNMENT, 2);
+    gl.pixelStorei(gl.PACK_ALIGNMENT, 2);
+
+    const { pixelData } = image;
+    const data = new Uint8Array(pixelData.buffer, pixelData.byteOffset, pixelData.byteLength);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.LUMINANCE_ALPHA, image.width, image.height, 0, gl.LUMINANCE_ALPHA, gl.UNSIGNED_BYTE, data);
+}
+
+function colorStatements16(signed = false) {
+    let statement = 'float color = t.r*255.0 + t.a*255.*256.;';
+
+    if (signed) {
+        statement += 'if(color > 32767.) color = color - 65536.;';
+    }
+
+    statement += 'v = vec4(color, color, color, 255.);';
+
+    return statement;
+}
+
 export const GrayscaleUint16Renderer: ImageRendererWebgl = {
-    colorStatements: 'float color = t.r*255.0 + t.a*255.*256.; v = vec4(color, color, color, 255.);',
+    colorStatements: colorStatements16(),
 
     match(image: Image) { return image.components === 1 && image.pixelData instanceof Uint16Array },
 
     // webgl
-    buildTexture(gl: WebGLRenderingContext, image: Image) {
-        gl.pixelStorei(gl.UNPACK_ALIGNMENT, 2);
-        gl.pixelStorei(gl.PACK_ALIGNMENT, 2);
-
-        const data = new Uint8Array(image.pixelData.buffer);
-        gl.texImage2D(gl.TEXTURE_2D, 0, gl.LUMINANCE_ALPHA, image.width, image.height, 0, gl.LUMINANCE_ALPHA, gl.UNSIGNED_BYTE, data);
-    }
+    buildTexture: buildTexture16
 }
 
 export const GrayscaleInt16Renderer: ImageRendererWebgl = {
-    colorStatements: `
-        float color = t.r*255.0 + t.a*255.*256.;
-        if(color > 32767.) color = color - 65536.;
-        v = vec4(color, color, color, 255.);
-    `,
+    colorStatements: colorStatements16(true),
 
     match(image: Image) { return image.components === 1 && image.pixelData instanceof Int16Array },
 
     // webgl
-    buildTexture(gl: WebGLRenderingContext, image: Image) {
-        gl.pixelStorei(gl.UNPACK_ALIGNMENT, 2);
-        gl.pixelStorei(gl.PACK_ALIGNMENT, 2);
-
-        const data = new Uint8Array(image.pixelData.buffer);
-        gl.texImage2D(gl.TEXTURE_2D, 0, gl.LUMINANCE_ALPHA, image.width, image.height, 0, gl.LUMINANCE_ALPHA, gl.UNSIGNED_BYTE, data);
-    }
+    buildTexture: buildTexture16
 }
 
 export const ColorUint8Renderer: ImageRendererWebgl = {
@@ -48,10 +53,11 @@ export const ColorUint8Renderer: ImageRendererWebgl = {
         gl.pixelStorei(gl.UNPACK_ALIGNMENT, 1);
         gl.pixelStorei(gl.PACK_ALIGNMENT, 1);
 
+        const { pixelData } = image;
         // see https://github.com/KhronosGroup/WebGL/issues/1533
-        const data = image.pixelData instanceof Uint8ClampedArray ?
-            new Uint8Array(image.pixelData.buffer) :
-            image.pixelData;
+        const data = pixelData instanceof Uint8ClampedArray ?
+            new Uint8Array(pixelData.buffer, pixelData.byteOffset, pixelData.byteLength) :
+            pixelData;
 
         gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, image.width, image.height, 0, gl.RGB, gl.UNSIGNED_BYTE, data);
     }
